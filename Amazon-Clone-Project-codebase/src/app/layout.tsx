2356a6c4c88ca2c2ff5f94f72975345079850465
Terexitariusstomp <terexmaps@gmail.com>
2025-10-08 16:06:58 -0300

    Update favicon to external URL

--- Amazon-Clone-Project-codebase/src/app/layout.tsx
+++ Amazon-Clone-Project-codebase/src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Amazone",
   description: "Amazone COOP30 - Temporary Autonomous Zone for cooperation, regeneration and culture",
   icons: {
-    icon: "/favicon.ico",
+    icon: "https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/test-clones/f9dbff3c-828d-4087-a500-734899d703f7-amazone-coop30-on77bsx-gamma-site/assets/images/ae65513d-3168-4f6d-b890-656785ecda48-1.png",
   },
 };
 
